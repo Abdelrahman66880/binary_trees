7d6a866301e78c6a89833e9cffcf7f0eae4ff0bd
Abdelrahman <abdofathi317@gmail.com>
2024-02-27 00:00:26 -0800

    Binary Tree In C

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,18 +1,23 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_node - Function that create a new node of the tree.
+ *@parent: is a pointer to the parent node of the node that have been created.
+ *@value: is the data to put in the new node.
+ * Return:Your function must return a pointer to the new node or NULL if no.
+*/
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *tmpnode = malloc(sizeof(binary_tree_t));
+	binary_tree_t *tmpnode = malloc(sizeof(binary_tree_t));
 
-    if (tmpnode == NULL)
-    {
-        return (NULL);
-    }
+	if (tmpnode == NULL)
+	{
+		return (NULL);
+	}
 
-    tmpnode->n = value;
-    tmpnode->parent = parent;
-    tmpnode->left = NULL;
-    tmpnode->right = NULL;
-
-    return (tmpnode);
+	tmpnode->n = value;
+	tmpnode->parent = parent;
+	tmpnode->left = NULL;
+	tmpnode->right = NULL;
+	return (tmpnode);
 }
