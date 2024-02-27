907caacbb653f026ef3e24e11d676a721a94660f
Abdelrahman <abdofathi317@gmail.com>
2024-02-27 08:06:57 -0800

    Binary Trees In C

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 
+
 /**
  * binary_tree_insert_left - a function that inserts a node as the left-child
  *@parent: a pointer to the node to insert the left-child in.
@@ -10,10 +11,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 
 	binary_tree_t *newNode = NULL;
+
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
+
 	newNode = malloc(sizeof(binary_tree_t));
 	if (newNode == NULL)
 	{
