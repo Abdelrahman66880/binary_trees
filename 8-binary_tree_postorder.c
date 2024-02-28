c9a082eb663f918357c1d29447dea504641497eb
Abdelrahman <abdofathi317@gmail.com>
2024-02-28 12:07:10 -0800

    Binary Trees In C

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 
+
 /**
  * binary_tree_postorder - Function that traverse postOrder.
  * @tree:  is a pointer to the root node of the tree to traverse.
@@ -9,8 +10,8 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 if (func && tree)
 {
-binary_tree_postorder(tree->right, func);
 binary_tree_postorder(tree->left, func);
+binary_tree_postorder(tree->right, func);
 func(tree->n);
 }
 }
